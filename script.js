bf3245a8c46992761a206c084b19074dfbd84dad
Prasanna1618r <prasannarithi1618@gmail.com>
2025-03-14 20:00:44 +0530

    Modified

--- script.js
+++
@@ -1,7 +0,0 @@
-function add(){
-    var b1 = Number(document.getElementById("k").value)
-    var b2 = Number(document.getElementById("k1").value)
-    document.getElementById("r").textContent=b1+b2
-}
- 
-// console.log(total)
